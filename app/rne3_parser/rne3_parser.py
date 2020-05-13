6fca1e5ca4ad4612f44cf48f9603a930ea7d1a9a
kingkastle <kingkastle4004@gmail.com>
2020-05-13 08:03:15 +0200

    update file

--- app/rne3_parser/rne3_parser.py
+++ app/rne3_parser/rne3_parser.py
@@ -29,6 +29,8 @@ class RNE3parser(Navigator):
     def _load(self):
         # pwd = os.getcwd()  # when running on local
         pwd = ''  # when running on heroku
+        print(pwd + self.backup_file_path)
+        print(os.path.isfile(pwd + self.backup_file_path))
         if os.path.isfile(pwd + self.backup_file_path):
             return pickle.load(open(pwd + self.backup_file_path, "rb"))
         else:
